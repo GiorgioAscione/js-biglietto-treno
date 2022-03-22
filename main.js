f71dc760464cf56e463b133692ab89ade09c826a
GiorgioAscione <acergio95@libero.it>
2022-03-22 17:54:01 +0100

    TERZO COMMIT

--- main.js
+++ main.js
@@ -7,14 +7,28 @@ console.log (chilometri);
 let prezzotragitto=chilometri*0.21;
 console.log(prezzotragitto);
 
-if (anni <18){
-    alert("Sei sotto i 18 anni d'età")
-}
+let scontoperminori=prezzotragitto*0.2
+console.log(scontoperminori);
 
-else if (anni >65){
-    alert ("Sei sopra i 65 anni d'età")
+let scontoperver65=prezzotragitto*0.4
+console.log(scontoperver65);
+
+let prezzofinale;
+
+
+if (anni <18){
+    alert("Sei sotto i 18 anni di età")
+    prezzofinale=prezzotragitto-scontoperminori
+} 
+
+else if (anni >=65){
+    alert ("Sei sopra i 65 anni di età")
+    prezzofinale=prezzotragitto-scontoperver65
 }
 
 else {
     alert("Non hai diritto a sconti")
+    prezzofinale=prezzotragitto
 }
+
+console.log(prezzofinale + " Questo è il prezzo riservato per te");
