6e0bedaa3cc8e5d1c92d87912deea62c74065b13
GiorgioAscione <acergio95@libero.it>
2022-03-22 16:48:24 +0100

    SECONDO COMMIT

---
+++ main.js
@@ -0,0 +1,22 @@
+let anni= prompt("Inserisci la tua età");
+console.log (anni);
+
+let chilometri= prompt("Inserisci il chilometraggio")
+console.log (chilometri);
+
+let prezzotragitto=chilometri*0.21;
+console.log(prezzotragitto);
+
+let prezzofinale=
+
+if (anni <18){
+    alert("Sei sotto i 18 anni d'età")
+}
+
+else if (anni >65){
+    alert ("Sei sopra i 65 anni d'età")
+}
+
+else {
+    alert("Non hai diritto a sconti")
+}
